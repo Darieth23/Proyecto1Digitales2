9c282e9bdd31c6eb0db50ae121e3c92d678dff36
Darieth Fonseca <zufon23@gmail.com>
2020-05-20 17:56:32 -0600

    cambios en el demux

--- Demux/Demux1a2_descp_condL1.v
+++ Demux/Demux1a2_descp_condL1.v
@@ -2,7 +2,7 @@
 module demux2a1_descp_condL1(
      //entradas
     input reset_L,
-    input selectorL1,
+    input [1:0] selectorL1,
     input valid0,
     input valid1,
     input [7:0] data_in0_demuxL1,
@@ -17,6 +17,19 @@ module demux2a1_descp_condL1(
     output wire [7:0] dataout2_muxL1,
     output wire [7:0] dataout3_muxL1
 );
-    demux1a2ochobits_descp_cond demux1a2_8bitunoL1(reset_L,selectorL1,valid0,data_in0_demuxL1,validout0,validout1,dataout0_muxL1,dataout1_muxL1);
-    demux1a2ochobits_descp_cond demux1a2_8bitdosL1(reset_L,selectorL1,valid1,data_in1_demuxL1,validout2,validout3,dataout2_muxL1,dataout3_muxL1);
-endmodule
+    always @ (*) begin
+        if(selectorL1[1] == 1) begin
+            demux1a2ochobits_descp_cond demux1a2_8bitdosL1(reset_L,selectorL1,valid1,data_in1_demuxL1,validout2,validout3,dataout2_muxL1,dataout3_muxL1);
+            valid0 = 0;
+            valid1 = 0;
+            dataout0_muxL1 = 8'bx;
+            dataout1_muxL1 = 8'bx;
+        end
+        else begin
+            demux1a2ochobits_descp_cond demux1a2_8bitunoL1(reset_L,selectorL1,valid0,data_in0_demuxL1,validout0,validout1,dataout0_muxL1,dataout1_muxL1);
+            valid2 = 0;
+            valid3 = 0;
+            dataout2_muxL1 = 8'bx;
+            dataout3_muxL1 = 8'bx;
+        end
+    endmodule
